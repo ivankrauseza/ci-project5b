6fcf84d8b772a9f76dcf2bebb8cf9f70e9558b1d
Ivan Krause <ivan.krause@gmail.com>
2024-05-17 17:09:58 +0100

    created all basic views, urls, and page templates required for the project in the Shop App

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,16 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.Home, name='Home'),
+    path('', views.Home, name='home'),
+    path('about/', views.About, name='about'),
+    path('contact/', views.Contact, name='contact'),
+    path('privacy-policy/', views.Privacy, name='privacy'),
+    path('customer-support/', views.Support, name='support'),
+    path('search/', views.ProductSearch, name='search'),
+    path('products/', views.ProductList, name='products'),
+    path('products/collection/', views.ProductCategories, name='collection'),
+    path('product/', views.ProductDetail, name='product-detail'),
+    path('basket/', views.Basket, name='basket'),
+    path('order-confirmation/', views.OrderConfirmation, name='confirm'),
+    path('dashboard/', views.Dashboard, name='Dashboard'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -5,3 +5,58 @@ from django.contrib import messages
 def Home(request):
     messages.info(request, 'Test Toast')
     return render(request, 'index.html')
+
+
+def About(request):
+    # messages.info(request, 'Test Toast')
+    return render(request, 'about.html')
+
+
+def Contact(request):
+    # messages.info(request, 'Test Toast')
+    return render(request, 'contact.html')
+
+
+def Privacy(request):
+    # messages.info(request, 'Test Toast')
+    return render(request, 'privacy.html')
+
+
+def Support(request):
+    # messages.info(request, 'Test Toast')
+    return render(request, 'support.html')
+
+
+def ProductSearch(request):
+    # messages.info(request, 'Test Toast')
+    return render(request, 'product-list-search.html')
+
+
+def ProductList(request):
+    # messages.info(request, 'Test Toast')
+    return render(request, 'product-list.html')
+
+
+def ProductCategories(request):
+    # messages.info(request, 'Test Toast')
+    return render(request, 'product-list-categories.html')
+
+
+def ProductDetail(request):
+    # messages.info(request, 'Test Toast')
+    return render(request, 'product-detail.html')
+
+
+def Basket(request):
+    # messages.info(request, 'Test Toast')
+    return render(request, 'basket.html')
+
+
+def OrderConfirmation(request):
+    # messages.info(request, 'Test Toast')
+    return render(request, 'order-confirmation.html')
+
+
+def Dashboard(request):
+    # messages.info(request, 'Test Toast')
+    return render(request, 'dashboard.html')

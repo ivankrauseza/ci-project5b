c5d802e70aea1d21ac3d3a32ab6f2bfb5e746af5
Ivan Krause <ivan.krause@gmail.com>
2024-05-27 15:25:22 +0100

    fix attempt: show debug in production to determine live error

--- cadence/settings.py
+++ cadence/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-24y2kyhh3u)bq!8g1a2qd_9s32oe3i(yl0lp9=or!@e1$j-zo$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 SITE_ID = 1
 
